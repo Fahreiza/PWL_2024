ddfa30cdc0ddbde0c6260156e41bb4921de62674
Fahreiza <114044591+Fahreiza@users.noreply.github.com>
2025-03-11 09:41:13 +0700

    commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>PWL 2025</title>
+</head>
+
+<body>
+    <h2>Pemrograman Web Lanjut 2025</h2>
+    <h3>Pemrograman Web Lanjut 2025</h3>
+</body>
+
+</html>
